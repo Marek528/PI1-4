fb7a1995987630863afc188a61394108c613ba4e
Marek528 <golias.marek.06@gmail.com>
2024-11-16 19:21:49 +0100

    upload tasks

---
+++ produkty.cs
@@ -0,0 +1,172 @@
+﻿using System.Globalization;
+
+namespace Produkty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("cs-CZ");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            string data = File.ReadAllText(args[0]);
+            Product[] products = ParseData(data);
+
+            Console.WriteLine("Produkty:");
+            foreach (var item in products)
+            {
+                if (!item.Quantity.HasValue)
+                {
+                    Console.WriteLine($"{item.Name}: neznáme množstvo; {item.Price} €");
+                }
+                else
+                {
+                    Console.WriteLine($"{item.Name}: {item.Quantity} ks; {item.Price} €");
+                }
+            }
+
+            double sum = GetTotalProductsPrice(products);
+            double averageWeight = GetAverageItemWeight(products);
+
+            Console.WriteLine($"\nCelková cena produktov: {sum} €");
+            Console.WriteLine($"Priemerná váha položky: {averageWeight} kg");
+        }
+
+        private static Product[] ParseData(string data)
+        {
+            string productName = "";
+            double price = 0;
+            int? quantity = 0;
+            Weight? weight = null;
+            int numberIndex;
+
+            List<Product> products = new List<Product>();
+            string[] lines = data.Split(['\n', '\r'], StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string line in lines)
+            {
+                string trimmedLine = line.Trim();
+                numberIndex = trimmedLine.IndexOfAny("0123456789".ToCharArray());
+
+                if (trimmedLine.StartsWith('-') && !trimmedLine.StartsWith("- price:") && !trimmedLine.StartsWith("- quantity:") && !trimmedLine.StartsWith("- weight:"))
+                {
+                    if (productName != "")
+                    {
+                        products.Add(new Product(productName, price, quantity, weight ?? new Weight(WeightUnit.g, 0)));
+                    }
+
+                    productName = trimmedLine.Trim('-').Trim(':').Trim();
+                    price = 0;
+                    quantity = null;
+                    weight = null;   
+                }
+                else if (trimmedLine.StartsWith("- price:"))
+                {
+                    price = double.Parse(trimmedLine.Substring(numberIndex), CultureInfo.InvariantCulture);
+                }
+                else if (trimmedLine.StartsWith("- quantity:"))
+                {
+                    quantity = int.Parse(trimmedLine.Substring(numberIndex), CultureInfo.InvariantCulture);
+                }
+                else if (trimmedLine.StartsWith("- weight:"))
+                {
+                    weight = ParseWeight(trimmedLine.Substring(numberIndex));
+                }
+            }
+
+            if (productName != "")
+            {
+                products.Add(new Product(productName, price, quantity, weight ?? new Weight(WeightUnit.g, 0)));
+            }
+
+            return products.ToArray();
+        }
+
+
+        private static Weight ParseWeight(string weight)
+        {
+            string[] parts = weight.Split(' ');
+            double value = double.Parse(parts[0], CultureInfo.InvariantCulture);
+            WeightUnit unit = (WeightUnit)Enum.Parse(typeof(WeightUnit), parts[1]);
+
+            return new Weight(unit, value);
+        }
+
+        private static double GetTotalProductsPrice(Product[] products)
+        {
+            double sum = 0;
+
+            foreach (var item in products)
+            {
+                sum += item.Price * (item.Quantity.HasValue ? item.Quantity.Value : 0);
+            }
+
+            return sum;
+        }
+
+        private static double GetAverageItemWeight(Product[] products)
+        {
+            double sum = 0;
+            int count = 0;
+
+            foreach (var item in products)
+            {
+                sum += Weight.GetNormalizedValue(item.Weight.Unit, item.Weight.Value);
+                count++;
+            }
+
+            return Math.Round(sum / count, 3);
+        }
+    }
+
+    class Product
+    {
+        public string Name { get; private set; }
+        public double Price { get; private set; }
+        public int? Quantity { get; private set; }
+        public Weight Weight { get; private set; }
+
+        public Product(string name, double price, int? quantity, Weight weight)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+            Weight = weight;
+        }
+    }
+
+    public struct Weight
+    {
+        public WeightUnit Unit { get; private set; }
+        public double Value { get; private set; }
+
+        public Weight(WeightUnit unit, double value)
+        {
+            Unit = unit;
+            Value = value;
+        }
+
+        public static double GetNormalizedValue(WeightUnit unit, double value)
+        {
+            if (unit == WeightUnit.g)
+            {
+                return value / 1000;
+            }
+            else if (unit == WeightUnit.dkg)
+            {
+                return value / 100;
+            }
+            else
+            {
+                return value;
+            }
+        }
+    }
+
+    public enum WeightUnit
+    {
+        kg,
+        dkg,
+        g
+    }
+}
---
+++ zadanie_1-2.cs
@@ -0,0 +1,149 @@
+﻿namespace Sample
+{
+    class Test
+    {
+        public static void Main(string[] args)
+        {
+            // 1. zadanie
+            // int[] bankovky = {500, 200, 100, 50, 20, 10, 5, 2, 1};
+            // int[] kusy = new int[9];
+            // int suma = 0;
+            // Console.WriteLine("Zadajte sumu na rozmenenie: ");
+            // suma = Int32.Parse(Console.ReadLine());
+
+            // for (int i = 0; i < bankovky.Length; i++)
+            // {
+            //     int zvysok = suma / bankovky[i];
+            //     suma -= bankovky[i] * zvysok;
+            //     kusy[i] = zvysok;
+            // }
+
+            // for (int i = 0; i < kusy.Length; i++)
+            // {
+            //     if (kusy[i] != 0)
+            //     {
+            //         Console.WriteLine($"{bankovky[i]} €: {kusy[i]}");
+            //     }
+            // }
+
+            // 2. zadanie
+            string[] vstup = Console.ReadLine().Split(' ');
+            int vyska = Int32.Parse(vstup[2]);
+            int sirka = Int32.Parse(vstup[1]);
+            int mod = Int32.Parse(vstup[0]);
+            string repeated;
+            int vnutro = sirka - 2;
+            string[] vypln = new string[vnutro];
+            string result;
+
+            if (mod == 0)
+            {
+                for (int i = 0; i < vyska; i++)
+                {
+                    repeated = new string('X', sirka);
+                    Console.WriteLine(repeated);
+                }
+            }
+            else if (mod == 1)
+            {
+                for (int i = 0; i < vyska; i++)
+                {
+                    if (i == 0 || i == vyska - 1)
+                    {
+                        repeated = new string('X', sirka);
+                        Console.WriteLine(repeated);
+                    }
+                    else
+                    {
+                        repeated = new string(' ', vnutro);
+                        Console.WriteLine($"X{repeated}X");
+                    }
+                }
+            }
+            else if (mod == 2)
+            {
+                int num = 0;
+                for (int i = 0; i < vyska; i++)
+                {
+                    if (i == 0 || i == vyska - 1)
+                    {
+                        repeated = new string('X', sirka);
+                        Console.WriteLine(repeated);
+                    }
+                    else
+                    {
+                        for (int j = 0; j < vnutro; j++)
+                        {
+                            if (num == 10)
+                            {
+                                num = 0;
+                            }
+                            vypln[j] = num.ToString();
+                            num++;
+                        }
+
+                        result = string.Join("", vypln);
+                        Console.WriteLine($"X{result}X");
+                    }
+                }
+            }
+            else if (mod == 3)
+            {
+                for (int i = 0; i < sirka; i++)
+                {
+                    repeated = new string(' ', i);
+                    Console.Write(repeated);
+                    Console.WriteLine("X");
+                }
+            }
+            else if (mod == 6)
+            {
+                for (int i = 0; i < vyska; i++)
+                {
+                    if (i == 0)
+                    {
+                        repeated = new string('X', sirka);
+                        Console.WriteLine(repeated);
+                    }
+                    else
+                    {
+                        repeated = new string(' ', (sirka - 1) / 2);
+                        Console.WriteLine($"{repeated}X");
+                    }
+                }
+            }
+            else if (mod == 9)
+            {
+                int num = 0;
+                for (int i = 0; i < vyska; i++)
+                {
+                    if (i == 0 || i == vyska - 1)
+                    {
+                        repeated = new string('X', sirka);
+                        Console.WriteLine(repeated);
+                    }
+                    else
+                    {
+                        for (int j = 0; j < vnutro; j++)
+                        {
+                            vypln[j] = num.ToString();
+                            num += vyska - 2;
+                            if (num >= 10)
+                            {
+                                num %= 10;
+                            }
+                        }
+
+                        result = string.Join("", vypln);
+                        Console.WriteLine($"X{result}X");
+                    }
+                    num = i;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wrong mod!");
+            }
+        }
+    }
+}
